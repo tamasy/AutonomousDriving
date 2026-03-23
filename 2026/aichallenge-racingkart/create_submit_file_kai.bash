542c61af46c0baf4be75ca2dc1ebc6c620a31bfa
tamasy <teamtamasy@gmail.com>
2026-03-23 23:57:30 +0900

    2026 mpc PR取込

---
+++ 2026/aichallenge-racingkart/create_submit_file_kai.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+tar zcvf "${SCRIPT_DIR}/submit/aichallenge_submit.tar.gz" \
+  -C "${SCRIPT_DIR}/aichallenge/workspace/src" aichallenge_submit \
+  -C "${SCRIPT_DIR}/aichallenge/memo" .
