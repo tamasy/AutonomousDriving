ca882b272059e881d59a679e6bbced4a922f8b54
tamasy <teamtamasy@gmail.com>
2026-03-27 23:15:45 +0900

    MPC完走設定

--- 2026/aichallenge-racingkart/create_submit_file_kai.bash
+++ 2026/aichallenge-racingkart/create_submit_file_kai.bash
@@ -4,4 +4,4 @@ SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
 tar zcvf "${SCRIPT_DIR}/submit/aichallenge_submit.tar.gz" \
   -C "${SCRIPT_DIR}/aichallenge/workspace/src" aichallenge_submit \
-  -C "${SCRIPT_DIR}/aichallenge/memo" .
+  -C "${SCRIPT_DIR}/aichallenge/tools" .
